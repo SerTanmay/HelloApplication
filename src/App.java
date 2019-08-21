7ce9c2bf3a6443e4b63c37331511f26a1efd65e7
Tanmay-PN <tanmay_naik@persistent.com>
2019-08-21 15:14:26 +0530

    Testing 123

--- src/App.java
+++ src/App.java
@@ -2,9 +2,6 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-        System.out.println("More text");
-        System.out.println("We are awesome!");
-        System.out.println("I am not bored with the training");
+        System.out.println("Testing 123!");
     }
 }
